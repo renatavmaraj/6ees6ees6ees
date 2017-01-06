02406c6d40de5388456559e3e37251ab209a636c
Renata Maraj <renatavmaraj@gmail.com>
2017-01-06 13:40:37 -0800

    Completed refactor to es6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(honeyPot){
+  super(honeyPot)
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+}
+
+makeHoney() {
+  this.honeyPot++;
+}
+
+giveHoney() {
+  this.honeyPot--;
+}
+
 };
